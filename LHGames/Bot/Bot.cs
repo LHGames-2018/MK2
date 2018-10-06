2dabc16cbe2d2d90e46f7e4c8a4d006fd73f981a
Thienng97 <duc-thien@sympatico.ca>
2018-10-06 13:46:12 -0400

    for merge

--- LHGames/Bot/Bot.cs
+++ LHGames/Bot/Bot.cs
@@ -9,6 +9,9 @@ namespace LHGames.Bot
         internal IPlayer PlayerInfo { get; set; }
         private int _currentDirection = 1;
 
+        private MapAnalyzedUnit[,] mapAnalyzeds = new MapAnalyzedUnit[20,20];
+
+        //mapAnalyzeds[0]
         internal Bot() { }
 
         /// <summary>
@@ -34,9 +37,11 @@ namespace LHGames.Bot
                 _currentDirection *= -1;
             }
 
+            analyseMap(map);
+
             var data = StorageHelper.Read<TestClass>("Test");
             Console.WriteLine(data?.Test);
-            return AIHelper.CreateMoveAction(new Point(_currentDirection, 0));
+            return AIHelper.CreateMoveAction(new Point(0, 1));
         }
 
         /// <summary>
@@ -45,6 +50,61 @@ namespace LHGames.Bot
         internal void AfterTurn()
         {
         }
+
+        internal void analyseMap(Map map){
+
+            //Start Map
+            instanciateMap();
+
+            int currentX = PlayerInfo.Position.X;
+            int currentY = PlayerInfo.Position.Y;
+
+            int beginArrayX = currentX - 10;
+            int beginArrayY = currentY - 10;
+
+            int endArrayX = currentX + 10;
+            int endArrayY = currentY + 10;
+
+            int counterX = 0;
+            int counterY = 0;
+
+            for(int x = beginArrayX; x < endArrayX; x++){
+                for(int y = beginArrayY; y < endArrayY; y++){
+
+                    mapAnalyzeds[counterX,counterY].positionY = y; 
+                    mapAnalyzeds[counterX,counterY].positionX = x;
+                    mapAnalyzeds[counterX,counterY].tileContent = map.GetTileAt(x,y);
+
+                    counterY++;
+                    //Console.Write("Position: " + x + ", " + y + "||" + map.GetTileAt(x,y) + " ");
+                }
+                counterY = 0;
+                counterX++;
+            }
+            
+            printMap();
+
+        }
+
+        internal void instanciateMap(){
+            for(int i = 0; i < 20; i++){
+                for(int j = 0; j < 20; j++){
+                    mapAnalyzeds[i,j] = new MapAnalyzedUnit(); 
+                }
+            }
+        }
+
+        internal void printMap(){
+            for(int i = 0; i < 20; i++){
+                for(int j = 0; j < 20; j++){
+                    Console.Write("Position: " + mapAnalyzeds[i,j].positionX + "," + mapAnalyzeds[i,j].positionY + " " + mapAnalyzeds[i,j].tileContent + "||");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
     }
 }
 
---
+++ LHGames/Bot/MapAnalyzed.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using LHGames.Helper;
+
+namespace LHGames.Bot{
+
+    public class MapAnalyzedUnit{
+
+        internal MapAnalyzedUnit(){}
+        public TileContent tileContent{get; set;}
+
+        public int positionX{get;set;}
+        public int positionY{get;set;}
+
+    }
+
+
+}
